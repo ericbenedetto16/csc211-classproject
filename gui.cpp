7e7410a4f953c7f08f7e7cec73e18f9dc2893bdc
CristianMatthew <43302701+CristianMatthew@users.noreply.github.com>
2019-10-17 19:34:49 -0400

    Update gui.cpp

--- gui.cpp
+++ gui.cpp
@@ -1,6 +1,14 @@
 #include "linetype.h"
-
 #include <windows.h>
+
+bool lineType::parallelTesting(float slopeOne, float slopeTwo) {
+	if (slopeOne == slopeTwo) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 void initializeGui() {
 	SetConsoleTitle("Classes Project"); 
