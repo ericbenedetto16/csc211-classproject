2c45258e836603524fd9e8c6bae9640854764b2d
Eric Benedetto <ericsplace3@gmail.com>
2019-10-17 20:11:44 -0400

    Update lineType.h

--- lineType.h
+++ lineType.h
@@ -5,34 +5,30 @@ using namespace std;
 
 void initializeGui();
 
-// This includes all of the libraries we need.
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 // #include <string>
 #include <fstream>
 
-#define skip cout << endl;
-
-using namespace std;
-
 class lineType {
-	public:
-		// Display the slope to make sure the slope value is correct.
-		void getSlope(double);
-		// Set the slope - X1, X2, Y1, Y2.
-		void setSlope(double, double);
-		// Boolean to check if the slope is perpendicular or not. False if it isn't.
-		bool perpendicular();
-		// Boolean to check if the slope if parallel or not. False if it isn't.
-		bool parallel();
-		// Boolean to check if the slopes are equal or not.
-		bool equal();
-		double findIntersection();
-		double Test(ifstream&);
-	private:
-		double slopeEquation();
-		double a, b, c;
-		double slope;
+public:
+	// Display the slope to make sure the slope value is correct.
+	void getSlope();
+	// Set the slope - X1, X2, Y1, Y2.
+	void setSlope(double, double);
+	// Boolean to check if the slope is perpendicular or not. False if it isn't.
+
+	bool perpendicular();
+	// Boolean to check if the slope if parallel or not. False if it isn't.
+	bool parallel();
+	// Boolean to check if the slopes are equal or not.
+	bool equal();
+	double findIntersection();
+	double Test(ifstream&);
+private:
+	double slopeEquation();
+	double a, b, c;
+	double slope;
 };
 #endif
