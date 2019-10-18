bd541bba60559245aa3cc960585c15ee079d557e
Eric Benedetto <ericsplace3@gmail.com>
2019-10-17 20:09:50 -0400

    Update class-structures.cpp

--- class-structures.cpp
+++ class-structures.cpp
@@ -1,31 +1,30 @@
-// This includes all of the libraries we need.
-#include <iostream>
-#include <cmath>
-#include <iomanip>
-// #include <string>
-#include <fstream>
+#include "linetype.h"
 
-#define skip cout << endl;
+void lineType::setSlope(double a, double b)
+{
+	if (a == 0) {
+		cout << "The slope is equal to 0";
+		skip;
+		slope = 0;
+		cout << "Your slope is horizontal!";
+		skip;
+	}
+	else if (b == 0) {
+		cout << "Your slope is vertical!";
+		skip;
+	}
+	else {
+		slope = a * -1 / b;
+		// cout << slope;
+		skip;
+	}
+}
 
-using namespace std;
-
-class lineType {
-public:
-	// Display the slope to make sure the slope value is correct.
-	void getSlope();
-	// Set the slope - X1, X2, Y1, Y2.
-	void setSlope(double, double);
-	// Boolean to check if the slope is perpendicular or not. False if it isn't.
-
-	bool perpendicular();
-	// Boolean to check if the slope if parallel or not. False if it isn't.
-	bool parallel();
-	// Boolean to check if the slopes are equal or not.
-	bool equal();
-	double findIntersection();
-	double Test(ifstream&);
-private:
-	double slopeEquation();
-	double a, b, c;
-	double slope;
-};
+// This function returns and displays the slope.
+void lineType::getSlope() {
+	cout << "This is your slope: ";
+	skip;
+	cout << slope;
+	skip;
+	cout << "Y = " << slope << "x + " << c;
+}
